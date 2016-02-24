8b482e18ff2f6036fdd60e22afa834bc90de2add
WeichaoLi <1303169452@qq.com>
2016-02-24 14:45:33 +0800

    f

--- NSThread/LWCViewController.m
+++ NSThread/LWCViewController.m
@@ -7,11 +7,21 @@
 //
 
 #import "LWCViewController.h"
+#import <pthread/pthread.h>
+#import <libkern/OSAtomic.h>
 
 #define HOST @"http://img2.moko.cc/users/14/4228/1268402/post/"
 
 @interface LWCViewController () {
     NSMutableArray *array;
+//    NSLock *lock;
+//    NSConditionLock *lock;
+//    NSRecursiveLock *lock;
+    NSCondition *condition;
+    
+    OSSpinLock spinLock;
+    
+    NSThread *thread1;
 }
 
 @end
@@ -28,23 +38,177 @@
     return self;
 }
 
+/////////////////////////////////////
+
+int i = 0;
+
+//- (void)testThread {
+//    for (int k = 0; k < 100; k++) {
+////        @synchronized(self){
+//        [lock lock];
+//            if (i < 100) {
+//                [NSThread sleepForTimeInterval:1];
+//                i++;
+//                NSLog(@"%i当前线程名称：%@",i,[[NSThread currentThread] name]);
+//            }else {
+//                [NSThread exit];
+//            }
+//        [lock unlock];
+////        }
+//    }
+//}
+
+//递归锁
+//- (void)testThread {
+//    NSRecursiveLock *recursivelock = [[NSRecursiveLock alloc] init];
+//    for (int k = 0; k < 100; k++) {
+//        [recursivelock lock];
+//            if (i < 100) {
+//                [NSThread sleepForTimeInterval:1];
+//                i++;
+//                NSLog(@"%i当前线程名称：%@",i,[[NSThread currentThread] name]);
+//            }else {
+//                [NSThread exit];
+//            }
+//        [recursivelock unlock];
+//    }
+//}
+
+////条件锁
+//- (void)testThread1 {
+//    for (int k = 0; k < 100; k++) {
+//        [lock lockWhenCondition:0];
+//        i++;
+//        NSLog(@"加=======%d",i);
+//        [lock unlockWithCondition:1];
+//    }
+//}
+//
+//- (void)testThread2 {
+//    for (int k = 0; k < 100; k++) {
+//        
+//        [lock lockWhenCondition:1];
+//        if (i > 0) {
+//            i--;
+//            NSLog(@"减=======%d",i);
+//        }
+//        [lock unlockWithCondition:0];
+//    }
+//}
+
+- (void)createConsumenr
+{
+    for (int k = 0; k < 100; k++) {
+        [condition lock];
+        while (i == 0) {
+            NSLog(@"=======等待");
+            [condition wait];
+        }
+        i--;
+        NSLog(@"减, %d", i);
+        [condition unlock];
+    }
+}
+
+- (void)createProducter
+{
+    for (int k = 0; k < 100; k++) {
+        [condition lock];
+        i += 2;
+        NSLog(@"加, %d", i);
+        [condition signal];
+        [condition unlock];
+    }
+}
+
+pthread_mutex_t mutex;
+
+//- (void)testThread {
+//    for (int k = 0; k < 100; k++) {
+//        pthread_mutex_lock(&mutex);
+//            if (i < 100) {
+////                [NSThread sleepForTimeInterval:0.5];
+//                i++;
+//                NSLog(@"%i当前线程名称：%@",i,[[NSThread currentThread] name]);
+//            }else {
+//                [NSThread exit];
+//                pthread_mutex_destroy(&mutex);
+//            }
+//        pthread_mutex_unlock(&mutex);
+//    }
+//}
+
+- (void)testThread {
+    for (int k = 0; k < 100; k++) {
+        if([[NSThread currentThread] isCancelled]) {
+            [NSThread exit];
+        }else {
+            OSSpinLockLock(&spinLock);
+            if (i < 100) {
+//                [NSThread sleepForTimeInterval:1];
+                i++;
+                NSLog(@"当前线程：%@", [NSThread currentThread]);
+//                NSLog(@"%i当前线程名称：%@",i,[[NSThread currentThread] name]);
+            }else {
+                [NSThread exit];
+            }
+            OSSpinLockUnlock(&spinLock);
+        }
+    }
+}
+
+
+- (void)startThread{
+    thread1 = [[NSThread alloc] initWithTarget:self selector:@selector(testThread)object:nil];
+    [thread1 setName:@"线程1"];
+    [thread1 start];
+    
+    NSThread *thread2 = [[NSThread alloc] initWithTarget:self selector:@selector(testThread)object:nil];
+    [thread2 setName:@"线程2"];
+    [thread2 start];
+    
+    NSThread *thread3 = [[NSThread alloc] initWithTarget:self selector:@selector(testThread)object:nil];
+    [thread3 setName:@"线程3"];
+    [thread3 start];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    if (![thread1 isCancelled]) {
+        [thread1 cancel];
+    }else {
+
+    }
+}
+
+
+/////////////////////////////////////
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIActionSheet *actionsheet = [[UIActionSheet alloc] initWithTitle:@"111" delegate:nil cancelButtonTitle:@"取消" destructiveButtonTitle:@"f" otherButtonTitles:@"yes", nil];
+    [actionsheet showInView:self.view];
+    /**
+     *  线程安全
+     */
+    pthread_mutex_init(&mutex, NULL);
+    spinLock = OS_SPINLOCK_INIT;
+    [self startThread];
     
     array = [NSMutableArray arrayWithObjects:@"40/img2_cover_9718626.jpg", @"6a/img2_src_9717514.jpg", @"3d/img2_src_9717515.jpg", @"2d/img2_src_9717518.jpg", @"d5/img2_src_9717517.jpg", @"f0/img2_src_9717516.jpg", @"f6/img2_cover_9645660.jpg",@"29/img2_src_9672505.jpg", @"d8/img2_src_9672506.jpg", @"d8/img2_src_9642133.jpg", @"d5/img2_src_9642134.jpg", @"6a/img2_src_9642135.jpg", @"da/img2_src_9642136.jpg", @"f0/img2_src_9583716.jpg", @"b6/img2_src_9583717.jpg", @"ee/img2_src_9583718.jpg", @"48/img2_src_9583719.jpg", @"b0/img2_src_9583720.jpg", nil];
 }
 
+
+
 - (void)loadView {
     [super loadView];
     
-    _myTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-    _myTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    _myTableView.delegate = self;
-    _myTableView.dataSource = self;
-    
-    [self.view addSubview:_myTableView];
+//    _myTableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+//    _myTableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+//    _myTableView.delegate = self;
+//    _myTableView.dataSource = self;
+//    
+//    [self.view addSubview:_myTableView];
 }
 
 - (void)didReceiveMemoryWarning
